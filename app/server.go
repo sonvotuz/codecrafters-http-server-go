352e5f86dba29fd1d55086aff1b76e5947d14ec2
Son Vo <vnsonvo@gmail.com>
2024-04-11 21:40:33 +0700

    respond with 404 when path not found

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,18 +13,42 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer listener.Close()
 
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		// _, err = fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+		// if err != nil {
+		// 	fmt.Println("Error writing response:", err.Error())
+		// 	os.Exit(1)
+		// }
+
+		go handleConnection(conn)
 	}
 
-	defer conn.Close()
+}
 
-	_, err = fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	req := make([]byte, 1024)
+	_, err := conn.Read(req)
 	if err != nil {
-		fmt.Println("Error writing response:", err.Error())
-		os.Exit(1)
+		fmt.Println("Failed to read from connection", err)
+		return
+	}
+
+	data := strings.Split(string(req), "\r\n")
+	path := strings.Split(data[0], " ")
+	if path[1] == "/" {
+		response := []byte("HTTP/1.1 200 OK\r\n\r\n")
+		conn.Write(response)
+	} else {
+		response := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+		conn.Write(response)
 	}
 }
