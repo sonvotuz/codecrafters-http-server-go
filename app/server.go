0fe63c72042e88a3098d2e0efbe613efaeef8bce
Son Vo <vnsonvo@gmail.com>
2024-04-11 22:36:55 +0700

    get a file and respond it

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -54,21 +55,38 @@ func handleConnection(conn net.Conn) {
 	} else if strings.HasPrefix(path[1], "/echo") {
 		randomStr := path[1][6:]
 
-		responseWithContent(conn, randomStr)
+		responseWithContent(conn, randomStr, "text/plain")
 	} else if strings.HasPrefix(path[1], "/user-agent") {
 		userAgent := strings.Split(data[2], " ")[1]
 
-		responseWithContent(conn, userAgent)
+		responseWithContent(conn, userAgent, "text/plain")
+	} else if strings.HasPrefix(path[1], "/files") && len(path[1][7:]) > 0 {
+		fileName := path[1][7:]
+		if _, err := os.Stat(fileName); err != nil {
+			response := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+			conn.Write(response)
+		} else {
+			var directoryFlagPtr = flag.String("directory", "", "define directory")
+
+			dataBytes, err := os.ReadFile(fmt.Sprintf("%s/%s", *directoryFlagPtr, fileName))
+			if err != nil {
+				response := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+				conn.Write(response)
+			}
+			data := string(dataBytes)
+			responseWithContent(conn, data, "application/octet-stream")
+		}
 	} else {
 		response := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
 		conn.Write(response)
 	}
 }
 
-func responseWithContent(conn net.Conn, data string) {
+func responseWithContent(conn net.Conn, data, contentType string) {
 	buf := bytes.Buffer{}
 	buf.WriteString("HTTP/1.1 200 OK\r\n")
 	buf.WriteString("Content-Type: text/plain\r\n")
+	buf.WriteString(fmt.Sprintf("Content-Type: %s\r\n", contentType))
 	buf.WriteString(fmt.Sprintf("Content-Length: %d\r\n\r\n", len(data)))
 	buf.WriteString(data)
 
